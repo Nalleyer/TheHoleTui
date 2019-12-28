0b04bc96c1385844775d2651f5c69f0de204ecb4
Nalleyer <nalleyer@gmail.com>
2019-12-28 17:00:37 +0800

    fps test ok

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{Error, stdout, Write},
+    io::{stdout, Error, Write},
     sync::mpsc,
     thread,
     time::Duration,
@@ -11,40 +11,45 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen},
 };
 
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Widget};
 use tui::{backend::CrosstermBackend, Terminal};
-use tui::widgets::{Widget, Block, Borders};
-use tui::layout::{Layout, Constraint, Direction};
-
 
 fn main() -> Result<(), Error> {
     let stdout = stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
     terminal.clear()?;
-    enable_raw_mode().expect("try enable raw mode");
-    terminal.draw(|mut f| {
-        let chunks = Layout::default()
-            .direction(Direction::Horizontal)
-            .margin(1)
-            .constraints(
-                [
-                    Constraint::Percentage(10),
-                    Constraint::Percentage(80),
-                    Constraint::Percentage(10)
-                ].as_ref()
-            )
-            .split(f.size());
-        Block::default()
-             .title("Block")
-             .borders(Borders::ALL)
-             .render(&mut f, chunks[0]);
-        Block::default()
-             .title("Block 1")
-             .borders(Borders::ALL)
-             .render(&mut f, chunks[1]);
-        Block::default()
-             .title("Block 2")
-             .borders(Borders::ALL)
-             .render(&mut f, chunks[2]);
-    })
+    // enable_raw_mode().expect("try enable raw mode");
+    terminal.hide_cursor()?;
+    let mut odd = true;
+    loop {
+        odd = !odd;
+        terminal.draw(|mut f| {
+            let chunks = Layout::default()
+                .direction(Direction::Horizontal)
+                .margin(1)
+                .constraints(
+                    [
+                        Constraint::Percentage(10),
+                        Constraint::Percentage(80),
+                        Constraint::Percentage(10),
+                    ]
+                    .as_ref(),
+                )
+                .split(f.size());
+            Block::default()
+                .title(if odd { "+" } else { "-" })
+                .borders(Borders::ALL)
+                .render(&mut f, chunks[0]);
+            Block::default()
+                .title("Block 1")
+                .borders(Borders::ALL)
+                .render(&mut f, chunks[1]);
+            Block::default()
+                .title("Block 2")
+                .borders(Borders::ALL)
+                .render(&mut f, chunks[2]);
+        })?;
+    }
 }
